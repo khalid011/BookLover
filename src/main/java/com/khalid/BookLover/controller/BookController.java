1dac4c17991e9c9097c7362b652b57f34e227c84
Md. Khalid Rahman <khalid.ducse@gmail.com>
2025-04-29 21:47:50 +0600

    [Title] Backend implementation of getting all books, updating and deleting a book in the system [done] [Description] - API implementation for getting all books - API implementation for updating a book - API implementation for deleting a book
    Signed-off-by: Md. Khalid Rahman <khalid.ducse@gmail.com>

--- src/main/java/com/khalid/BookLover/controller/BookController.java
+++ src/main/java/com/khalid/BookLover/controller/BookController.java
@@ -5,6 +5,8 @@ import com.khalid.BookLover.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class BookController {
     @Autowired
@@ -23,5 +25,20 @@ public class BookController {
     @PostMapping("/addBook")
     public String addNewBook(@RequestBody Book bookInfo) {
         return bookService.saveBook(bookInfo);
+    }
+
+    @GetMapping("/getAllBooks")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping("/updateBook/{id}")
+    public String updateBook(@RequestBody Book bookInfo, @PathVariable int id) {
+        return bookService.updateBook(bookInfo, id);
+    }
+
+    @PostMapping("/deleteBook/{id}")
+    public String deleteBook(@PathVariable int id) {
+        return bookService.deleteBook(id);
     }
 }
--- src/main/java/com/khalid/BookLover/service/BookService.java
+++ src/main/java/com/khalid/BookLover/service/BookService.java
@@ -5,6 +5,8 @@ import com.khalid.BookLover.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
     @Autowired
@@ -16,6 +18,29 @@ public class BookService {
             return "Book named " + bookInfo.getBookName() + " - is saved successfully in the db";
         } catch (Exception ex) {
             return "Failure, exception occurred" + ex.toString();
+        }
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public String updateBook(Book bookInfo, int id) {
+        try {
+            bookRepository.deleteById(id);
+            bookRepository.save(bookInfo);
+            return "Successfully updated the book titled:" + bookInfo.getBookName();
+        } catch (Exception e) {
+            return "Could not update the book, exception occured: " + e;
+        }
+    }
+
+    public String deleteBook(int id) {
+        try {
+            bookRepository.deleteById(id);
+            return "Successfully delete the book";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
